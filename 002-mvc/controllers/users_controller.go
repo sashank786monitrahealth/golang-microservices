b2a1aa394397f9f6b41356a79c99a0cafd67978f
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-11-25 13:32:16 +0530

    accepting queryParameters

--- 002-mvc/controllers/users_controller.go
+++ 002-mvc/controllers/users_controller.go
@@ -1,9 +1,12 @@
 package controllers
 
 import (
+	"log"
 	"net/http"
 )
 
 func GetUser(resp http.ResponseWriter, req *http.Request) {
-
+	var userID string = req.URL.Query().Get("user_id")
+	log.Printf("About to process user_id %v\n", userID)
+	log.Printf("About to process user_id %T\n", userID)
 }
