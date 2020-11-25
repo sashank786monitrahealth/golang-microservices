29ea8645ffd5bd7fc3f5c55a39b8ce760e35b27b
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-11-25 13:25:54 +0530

    curl localhost:8080/users -v

--- 002-mvc/app/app.go
+++ 002-mvc/app/app.go
@@ -1,12 +1,20 @@
-package app;
+package app
 
-import(
-   "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	// local module
+	"../controllers"
 )
 
+func StartApp() {
+	http.HandleFunc("/users", controllers.GetUser)
+	fmt.Printf("Inside app package.\n")
 
-func StartApp(){
-
-   fmt.Printf("Inside app package.\n");
+	var err error
+	if err = http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
 
 }
---
+++ 002-mvc/controllers/users_controller.go
@@ -0,0 +1,9 @@
+package controllers
+
+import (
+	"net/http"
+)
+
+func GetUser(resp http.ResponseWriter, req *http.Request) {
+
+}
